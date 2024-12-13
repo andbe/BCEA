f0aa4db79ddfd00c3d2a69daf0d3260ead423958
andbe <and.be.like@gmail.com>
2024-12-13 20:32:11 +0000

    Revert "Merge pull request #6 from andbe/dev-BCEAweb"
    This reverts commit 6233350d93d33000f14dae478ea964c8a0ac961f, reversing
    changes made to fb7b1cfe72fceacb3b6384fec2886d50086bcad8.

--- data-raw/smoking_parameters.R
+++
@@ -1,8 +0,0 @@
-# smoking_parameters
-
-library(BCEA)
-data(Smoking)
-
-pi_post |> as.data.frame() |> 
-  (\(x) setNames(x, paste0("p", 1:ncol(x))))() |> 
-  write.csv(file = "./data/smoking_parameters.csv", row.names = FALSE)
--- data-raw/vaccine_parameters.R
+++
@@ -1,7 +0,0 @@
-# vaccine_parameters
-
-library(BCEA)
-data(Vaccine)
-
-vaccine_mat |> as.data.frame() |> 
-  write.csv(file = "./data/vaccine_parameters.csv", row.names = FALSE)
